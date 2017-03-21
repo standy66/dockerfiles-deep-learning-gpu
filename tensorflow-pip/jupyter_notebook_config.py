a2ef88617743fd24c0419b5f6408d5dcd558639b
Andrew Stepanov <karn9050@gmail.com>
2017-03-21 16:58:24 +0300

    Remove tokens for standy/tensorflow images

--- tensorflow-pip/jupyter_notebook_config.py
+++ tensorflow-pip/jupyter_notebook_config.py
@@ -19,6 +19,8 @@ c.NotebookApp.ip = '*'
 c.NotebookApp.port = int(os.getenv('PORT', 8888))
 c.NotebookApp.open_browser = False
 c.MultiKernelManager.default_kernel_name = 'python2'
+c.NotebookApp.token = ''
+c.NotebookApp.password = ''
 
 # sets a password if PASSWORD is set in the environment
 #if 'PASSWORD' in os.environ:
